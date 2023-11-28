d29ce3eebea4d6bedb373e807b26d7645eaaf154
Muhammad-Zaorez <147588674+Muhammad-Zaorez@users.noreply.github.com>
2023-11-28 11:08:30 -0800

    Add files via upload

---
+++ HOME TASK 05.cpp
@@ -0,0 +1,77 @@
+/*
+                                 LAB MANUAL 05(HOME TASKS)
+                                         M.ZAOREZ(455443)
+                                            SEC A 
+*/
+//                                    TASK 01
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*int i, n1, n2, j, hcf = 1, lcm;
+    cout << " LCM of two numbers:\n";
+    cout << "------------------------\n";
+    cout << " Input 1st number for LCM: ";
+    cin >> n1;
+    cout << " Input 2nd number for LCM: ";
+    cin >> n2;
+    j = (n1 < n2) ? n1 : n2;
+    for (i = 1; i <= j; i++) {
+
+        if (n1 % i == 0 && n2 % i == 0) {
+            hcf = i;
+        }
+    }
+    lcm = (n1 * n2) / hcf;
+    cout << " The LCM of " << n1 << " and " << n2 << " is: " << lcm << endl;
+*/
+//                                      TASK 02
+
+    /*int n, a, d;
+
+    cout << "Enter the number of terms (n): ";
+    cin >> n;
+
+    cout << "Enter the first term (a): ";
+    cin >> a;
+
+    cout << "Enter the common difference (d): ";
+    cin >> d;
+
+    int sum = (n * (2 * a + (n - 1) * d)) / 2;
+
+    cout << "Sum of the arithmetic progression series: " << sum << endl;
+*/
+
+
+//                              TASK 03
+     /*int n;
+
+    cout << "Enter the number of rows in the diamond: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }*/
+
+
+
+}
+                                            
